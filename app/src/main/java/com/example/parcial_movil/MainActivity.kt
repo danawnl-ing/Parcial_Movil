9d603bd70f1ef3675d2e36cbc2390ba35cb525dd
Daniel_Guerra <guerragiraldodaniel@gmail.com>
2026-04-10 14:32:25 -0500

    Register and provisional home page created✨

--- app/src/main/java/com/example/parcial_movil/MainActivity.kt
+++ app/src/main/java/com/example/parcial_movil/MainActivity.kt
@@ -6,12 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.parcial_movil.ui.theme.Parcial_MovilTheme
+import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import com.example.parcial_movil.ui.theme.Red
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +30,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Parcial_MovilTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                var isRegistered by remember {mutableStateOf(false)}
+                if (isRegistered){
+                    HomeScreen()
+                }else{
+                    RegisterScreen (
+                        onRegisterSuccess = {
+                            isRegistered = true
+                        }
                     )
                 }
             }
@@ -31,17 +46,66 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    fun RegisterScreen(onRegisterSuccess: () -> Unit) {
 
+        var username by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var error by remember { mutableStateOf("") }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Text(text = "Registro")
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Usuario") }
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Contraseña") },
+                visualTransformation = PasswordVisualTransformation()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {
+                if (username.isNotEmpty() && password.isNotEmpty()) {
+                    onRegisterSuccess()
+                } else {
+                    error = "Debe llenar todos los campos"
+                }
+            }) {
+                Text("Ingresar")
+            }
+
+            if (error.isNotEmpty()) {
+                Text(text = error, color = Red)
+            }
+        }
+    }
+
+
+// Esta es una función provisional hasta que se haga la ventana de mediciones a la que se ingresa
+// apenas se oprime el boton (las de medidas)
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Parcial_MovilTheme {
-        Greeting("Android")
+fun HomeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Bienvenido 🎉")
     }
 }
