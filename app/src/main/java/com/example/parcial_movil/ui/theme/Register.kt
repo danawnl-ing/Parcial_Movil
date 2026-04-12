c98fe437d0e3ec06a81d586682f35cb3d71c41fd
Daniel_Guerra <guerragiraldodaniel@gmail.com>
2026-04-12 15:11:36 -0500

    Register file added

---
+++ app/src/main/java/com/example/parcial_movil/ui/theme/Register.kt
@@ -0,0 +1,70 @@
+package com.example.parcial_movil.ui.theme
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RegisterScreen(onRegisterSuccess: () -> Unit) {
+
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var error by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(text = "Registro")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Usuario") }
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Contraseñaaa") },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            if (username.isNotEmpty() && password.isNotEmpty()) {
+                onRegisterSuccess()
+            } else {
+                error = "Debe llenar todos los campos"
+            }
+        }) {
+            Text("Ingresar")
+        }
+
+        if (error.isNotEmpty()) {
+            Text(text = error, color = Red)
+        }
+    }
+}
